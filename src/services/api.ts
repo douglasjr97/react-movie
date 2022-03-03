61dd5eb6d4cca09b6f275ec854ec09be9af812a1
Douglas Falcao <falcao.douglas97@gmail.com>
2022-03-03 12:51:08 -0300

    fix: adjust token and endpoint

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
-const { TMDB_BEARER_TOKEN } = process.env;
 const baseUrlImages = 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/';
 const languageApi = '?language=pt-BR';
 
 const api = axios.create({
     baseURL: 'https://api.themoviedb.org/3',
-    headers: { Authorization: 'Bearer ' + TMDB_BEARER_TOKEN }
+    headers: {
+        Authorization:
+            'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMzg5YTZmZTdjNTZkMzJlODliMmM4YmZiY2UwODBmNCIsInN1YiI6IjYyMjBkNjRkOWYxYmU3MDAxYjFiMDgwZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PsJSzczD7X7Aa13a7Hb52a6bGplRBexJ_xZlEr3K7mc'
+    }
 });
 
 export { api, baseUrlImages, languageApi };
